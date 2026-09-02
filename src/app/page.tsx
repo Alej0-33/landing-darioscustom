8972b12a6303aa03f1d4e0abe99b75485f1e7cf4
Alej0-33 <alejandrogarces1991@gmail.com>
2026-09-02 16:43:46 -0400

    fix: deleted the page redirection on code now it works on cloudflare

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  // En 'output: export', esto genera un index.html con un <meta refresh> instantáneo.
-  // Cuando alguien entre a dariosironart.com será enviado automáticamente a la vista 
-  // estilizada e indexada en dariosironart.com/en
-  redirect('/en');
-}
